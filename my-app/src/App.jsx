45f6cbbbde7f11089d74687cb8c9960ca38be71c
Matheus S. Anselmo <169082718+Ans3lmo@users.noreply.github.com>
2024-06-25 23:46:51 +0000

    Update app imports

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,8 +2,8 @@ import './App.css';
 
 import { Navbar } from './Components/Navbar/Navbar';
 import { Footer } from './Components/Footer/Footer';
-import { Profile } from './Components/Profile/Profile';
-import { Social } from './Components/Social/Social';
+import { Profile } from './Components/landingPage/Profile/Profile';
+import { Social } from './Components/landingPage/Social/Social';
 
 export function App() {
   return (
