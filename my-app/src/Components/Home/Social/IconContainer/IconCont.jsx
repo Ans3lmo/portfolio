7f7b421ab600b4d13b4b7a19bf54f8ebd1643712
Ans3lmo <anselmoma2005@outlook.com>
2024-08-01 15:20:15 -0300

    Update inconCont.jsx - lines 7 and 9 | added noreferrer property on link component

--- my-app/src/Components/Home/Social/IconContainer/IconCont.jsx
+++ my-app/src/Components/Home/Social/IconContainer/IconCont.jsx
@@ -4,8 +4,8 @@ import './iconcont.css';
 export class IconCont extends React.Component {
   render(){
     return (
-      <a id="iconContainer" href={this.props.iconLink} target="_self">
-        <img src={this.props.icon} alt=""/>
+      <a id="iconContainer" href={this.props.iconLink} target="_blank" rel="noopener noreferrer">
+        <img src={this.props.icon} alt="icon logo"/>
         <span className="iconText" >{this.props.iconTitle}</span>
       </a>
     )
